f24fe76c770f2293749902b52e41cbe0e67095e2
bala-amypo <bala@amypo.in>
2025-12-26 14:26:31 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -2,42 +2,27 @@ package com.example.demo.model;
 
 import com.example.demo.exception.BadRequestException;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "categories")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+
     public static final String TYPE_INCOME = "INCOME";
     public static final String TYPE_EXPENSE = "EXPENSE";
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(unique = true)
+
     private String name;
-    
     private String type;
-    
-    public Category() {}
-    
-    public Category(Long id, String name, String type) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-    }
-    
+
     public void validateType() {
         if (!TYPE_INCOME.equals(type) && !TYPE_EXPENSE.equals(type)) {
-            throw new BadRequestException("Type must be INCOME or EXPENSE");
+            throw new BadRequestException("Invalid category type");
         }
     }
-    
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
 }
