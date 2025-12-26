5f1fb2682e8fc92582c3782147e4e4545bf717c7
bala-amypo <bala@amypo.in>
2025-12-26 15:44:31 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,20 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9315.408procr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Personal Finance Budget Planner API")
+                        .version("1.0")
+                        .description("API documentation for Personal Finance Budget Planner"));
+    }
 }
