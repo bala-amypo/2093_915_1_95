76bd12f88dd3e859a703ba5c422453c550c72e94
bala-amypo <bala@amypo.in>
2025-12-26 17:29:06 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.BudgetPlan;
 import com.example.demo.repository.BudgetPlanRepository;
 import com.example.demo.service.BudgetPlanService;
@@ -14,14 +13,12 @@ public class BudgetPlanServiceImpl implements BudgetPlanService {
     }
 
     @Override
-    public BudgetPlan createOrUpdate(BudgetPlan plan) {
-        plan.validate();
-        return repository.save(plan);
+    public BudgetPlan createOrUpdate(BudgetPlan budgetPlan) {
+        return repository.save(budgetPlan);
     }
 
     @Override
     public BudgetPlan getPlan(int month, int year) {
-        return repository.findByMonthAndYear(month, year)
-                .orElseThrow(() -> new BadRequestException("Plan not found"));
+        return repository.findByMonthAndYear(month, year);
     }
 }
