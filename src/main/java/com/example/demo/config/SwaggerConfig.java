5f41a93db344fd270df4032a05bd831022ca3bbf
bala-amypo <bala@amypo.in>
2025-12-27 04:16:56 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,18 +2,27 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local Development Server");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Personal Finance Budget Planner API")
                         .version("1.0")
-                        .description("API for managing personal finances and budgets"));
+                        .description("API documentation for Personal Finance Budget Planner System"))
+                .servers(List.of(localServer));
     }
 }
